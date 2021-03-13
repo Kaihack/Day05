0f64feebb3b5164f239e4292ece845d02c46f85d
Kaihack <prostor.za.rad@gmail.com>
2021-03-13 10:04:01 +0100

    Added index.js - Word Blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    // Only change code bellow this code
+    
+    var myNoun = "dog";
+    var myVerb = "run";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    wordBlanks = myNoun + " " + "Jackie" + " " + myVerb + " " + myAdjective + " " + myAdverb + ".";
+
+    // Only change code above this code
+    return wordBlanks;
+}
+
+
+console.log(main());
+module.exports = main 
