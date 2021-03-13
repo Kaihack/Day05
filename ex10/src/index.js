d1d3a15d8e3fbe3c7a5b2e6f2e245307bf388d5a
Kaihack <prostor.za.rad@gmail.com>
2021-03-13 09:57:59 +0100

    Added index.js - Understand String Immutability

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code bellow this line
+    
+    var myString = "Jello World";
+    var myString = "Hello World";
+
+    // Only change code above this line
+    return myString;
+}
+
+
+console.log(main());
+module.exports = main 
